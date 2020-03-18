363d3794fdc87d23a19e896fcef78e1f1aac27e1
Vivian Li <vivianli630@gmail.com>
2020-03-18 22:14:25 +0800

    refactor: Refactor code

--- demo/location-based/script.js
+++ demo/location-based/script.js
@@ -4,17 +4,17 @@ window.onload = () => {
 
 const models = [
   {
-    url: './../../assets/magnemite/scene.gltf',
+    url: './assets/magnemite/scene.gltf',
     scale: '0.5 0.5 0.5',
     rotation: '0 225 0'
   },
   {
-    url: './../../assets/articuno/scene.gltf',
+    url: './assets/articuno/scene.gltf',
     scale: '0.2 0.2 0.2',
     rotation: '0 225 0'
   },
   {
-    url: './../../assets/dragonite/scene.gltf',
+    url: './assets/dragonite/scene.gltf',
     scale: '0.08 0.08 0.08',
     rotation: '0 225 0'
   },
